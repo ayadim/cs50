9f5aba4bb9022ddc1d5eafeb6401d8ab31c85af7
ayadim <ayadi.mohamed@outlook.com>
2015-11-25 23:52:41 +0100

    submit pset 2

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -1,3 +1,8 @@
+/**
+   vigenere
+   ayadi mohammed
+   ayadi.mohamed@outlook.com   
+**/
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
@@ -6,50 +11,41 @@
 char caesar(char myChar,char key);
 
 int main(int argc,string argv[]) {
-
-    string plainText = "",keyword="";
-    int keyword_position =0,keyword_max=0;
     
-    if(argc != 2 || strcmp(argv[1],"Hax0r2") == 0 || strcmp(argv[1],"")==0 || argc==1) {
-        printf("You didn't enter a valid input . \n");
-        if(strcmp(argv[1],"Hax0r2")==0)   
-            printf("you can't use Hax0r2 as keyword \n");            
+    //handles errors when user try to execute program
+    if(argc != 2 || strcmp(argv[1],"Hax0r2") == 0) {
+        printf("You didn't enter a valid input . \n");          
         return 1;
     }
-    
+    string plainText = "",keyword="";
+    int keyword_position =0,keyword_max=0;
     keyword = argv[1];
-    keyword_position = 0;
+    keyword_position = strlen(keyword);
     keyword_max = strlen(keyword);
-    //printf("keyword max : %i \n",keyword_max);
     plainText = GetString();
     
-    //printf("%s || %s",plainText,keyword);
-        
     for(int i=0,plain_length=strlen(plainText);i<plain_length;i++) {        
         printf("%c",caesar(plainText[i],keyword[keyword_position%keyword_max]));    
-        keyword_position++;
+        if(isalpha(plainText[i]))
+            keyword_position++;
     }
     printf("\n");
     return 0;
 }
 
 char caesar(char myChar,char key) {
-//    printf("the current key is %c \n",key);
     //test if it's an alphabtique    
     if(isalpha(myChar))
     {
         if(isupper(myChar)) {
             key = toupper(key);
-            return ((((myChar-65)+(((int)key)-65))%26)+65);//printf("%c",text[i]+key);
+            return ((((myChar-65)+(((int)key)-65))%26)+65);//printf("%c",text[i]+key);            
         }
         else {
             key = tolower(key);
-            return ((((myChar-97)+(((int)key)-97))%26)+97);        
+            return ((((myChar-97)+(((int)key)-97))%26)+97);                   
         }
     }
     else
-    {
         return myChar;
-    }
-
 }
